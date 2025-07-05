8864de3ed69107f2e8df2d308a809ab07daedd16
Karl Gauck <kgauck@gmx.de>
2025-07-05 21:27:53 +0200

    gemini can be prompted to create certain cards

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import ollama from 'ollama/browser'
+import { GoogleGenAI } from '@google/genai'
+
 
 // Remember to rename these classes and interfaces!
 
@@ -14,121 +17,82 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: 'generate-flashcard',
+			name: 'Generate Flashcards',
+			callback: async () => {
+				const file = this.app.workspace.getActiveFile()
+				if (!file) return
+
+				const data = await this.app.vault.read(file)
+
+				const folder = file.parent?.path
+				if (!folder) return
+
+				console.log(data)
+				await this.query_gemini("Ignore everything of the following that is not explicitly marked as relevant: \n" + data).then (response => {
+					const flashcardFilePath = folder + "/flashcards.md"
+					if (this.app.vault.getFolderByPath(flashcardFilePath) == null)
+						this.app.vault.create(flashcardFilePath, response)
+					else this.app.vault.append(<TFile>this.app.vault.getFileByPath(flashcardFilePath), response)
+				}).catch(error => {
+					new Notice("error")
+				})
+				ollama.ps().then(console.log)
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		})
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		console.log(this.app.workspace.getActiveFile()?.name);
 	}
 
 	onunload() {
-
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	async query(context_data: string): Promise<string>  {
+		new Notice("Generating flashcards...")
+		const query = {
+			model: "cards",
+			prompt: context_data,
+			think: false
+		}
+		new Notice("Flashcards generated!")
+		return (await ollama.generate(query)).response
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	async query_gemini(context_data: string): Promise<string>  {
+		console.log('BRO')
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		const ai = new GoogleGenAI({apiKey: 'AIzaSyCYYxKezxXtoMONTsb9GvddFCFvAD0dWE8'})
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		console.log('HEY!')
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		const response = await ai.models.generateContent({
+			model: 'gemini-2.5-flash',
+			contents: context_data,
+			config: {
+				systemInstruction: "You should generate flashcards about all of the topics specified by the user as being relevant." +
+					"If there are no relevant topics, meaning, there is no sentence that explicitly asks for a flashcard, return an empty string." +
+					"Asking for a flashcard are stated in the following format:" +
+					"" +
+					"card: < information about which topics are relevant >" +
+					"" +
+					"When generating a flashcard, do it with the following format:" +
+					"```anki" +
+					"< Header / Title of flashcard >" +
+					"===" +
+					"< Content / Body of flashcard >" +
+					"```" +
+			}
+		})
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		console.log('HI')
+		console.log(response.promptFeedback)
+		if (!response.text)
+			return "error"
+		return response.text
 	}
 }
+
+
+
+
